ff9db6f44fd974ea10e04d2141008b2e4ebb039a
wxcican <wxcican@qq.com>
2017-03-15 17:47:21 +0800

    实现登录，注册UI

---
+++ app/src/main/java/com/fuicuiedu/xc/videonew_20170309/ui/likes/LoginFragment.java
@@ -0,0 +1,65 @@
+package com.fuicuiedu.xc.videonew_20170309.ui.likes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.fuicuiedu.xc.videonew_20170309.R;
+import com.fuicuiedu.xc.videonew_20170309.commons.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ * Created by Administrator on 2017/3/15 0015.
+ */
+
+public class LoginFragment extends DialogFragment{
+
+    private Unbinder mUnbinder;
+
+    @BindView(R.id.etUsername)
+    EditText mEtUsername;
+    @BindView(R.id.etPassword)
+    EditText mEtPassword;
+    @BindView(R.id.btnLogin)
+    Button mBtnLogin;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //无标题栏
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        View view = inflater.inflate(R.layout.dialog_login, container, false);
+        mUnbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onClick(){
+        String username = mEtUsername.getText().toString();
+        String password = mEtPassword.getText().toString();
+        //用户名和密码不能为空
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            ToastUtils.showShort(R.string.username_or_password_can_not_be_null);
+            return;
+        }
+
+        // TODO: 2017/3/15 0015 登录的网络请求
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mUnbinder.unbind();
+    }
+}
---
+++ app/src/main/java/com/fuicuiedu/xc/videonew_20170309/ui/likes/RegisterFragment.java
@@ -0,0 +1,61 @@
+package com.fuicuiedu.xc.videonew_20170309.ui.likes;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import com.fuicuiedu.xc.videonew_20170309.R;
+import com.fuicuiedu.xc.videonew_20170309.commons.ToastUtils;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Administrator on 2016/12/21 0021.
+ */
+
+public class RegisterFragment extends DialogFragment {
+
+    @BindView(R.id.etUsername)
+    EditText mEtUsername;
+    @BindView(R.id.etPassword)
+    EditText mEtPassword;
+    @BindView(R.id.btnRegister)
+    Button mBtnRegister;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //取消标题栏
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        View view = inflater.inflate(R.layout.dialog_register,container,false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void onClick(){
+        final String username = mEtUsername.getText().toString();
+        String password = mEtPassword.getText().toString();
+
+        //用户名和密码不能为空
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            ToastUtils.showShort(R.string.username_or_password_can_not_be_null);
+            return;
+        }
+
+        // TODO: 2017/3/15 0015 注册的网络请求
+    }
+
+}
