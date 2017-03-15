6f67791ede9363b7d5e6ee72e789ee90e59d93ec
wxcican <wxcican@qq.com>
2017-03-15 16:58:13 +0800

    MainActivity，实现

---
+++ app/src/main/java/com/fuicuiedu/xc/videonew_20170309/ui/news/NewsFragment.java
@@ -0,0 +1,27 @@
+package com.fuicuiedu.xc.videonew_20170309.ui.news;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.fuicuiedu.xc.videonew_20170309.R;
+
+/**
+ * Created by Administrator on 2017/3/15 0015.
+ */
+
+public class NewsFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_test,container,false);
+        TextView textView = (TextView) view.findViewById(R.id.fragment_test_tv);
+        textView.setText("在线新闻Fragment（待实现）");
+        return view;
+    }
+}
